d23bcd598939bd8fe19db36c7baddfb5273a2c1e
michaelveloso <mjv.audio@gmail.com>
2015-08-12 14:30:03 -0400

    making sure everything works

--- server.rb
+++ server.rb
@@ -4,19 +4,6 @@ require_relative 'class_league'
 require 'json'
 require 'pry'
 
-def find_teams
-  # ...
-  # returns teams
-  file = File.read('roster.json')
-  kickball_league = JSON.parse(file)
-  league = League.new("Kickball")
-  position_list = kickball_league.values[0].keys
-  kickball_league.each do |team, positions|
-    league.teams[team] = Team.new(team, positions)
-  end
-  league
-end
-
 file = File.read('roster.json')
 kickball_league = JSON.parse(file)
 league = League.new("Kickball")
